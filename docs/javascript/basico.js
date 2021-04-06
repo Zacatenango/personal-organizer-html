ce4f14e955da7c80d3c72827db20043cca945df1
Zacatenango <zacatenango@lalala>
2021-04-06 08:59:57 -0500

    Voy ahora a probar con web workers

--- docs/javascript/basico.js
+++ docs/javascript/basico.js
@@ -2,11 +2,9 @@
 
 $("#donGallevante").click(function()
 {
-   //$(this).text("Don Gallevante");
    var firebaseDB = firebase.database();
    // Para cargar el item raíz de mi DB, es con firebaseDB.ref() con parámetro en blanco
    var datos = firebaseDB.ref().child("kk").get();
-   $(this).text(datos);
+   $(this).text(`firebaseDB[kk] = ${datos}`);
 });
-
 
