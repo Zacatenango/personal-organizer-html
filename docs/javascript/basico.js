a7a3fba74334e002fa7708badf8089e731d55d30
Zacatenango <zacatenango@lalala>
2021-04-06 10:15:48 -0500

    Resulta que Firebase funciona en base a Promises, que requieren un manejo especial

--- docs/javascript/basico.js
+++ docs/javascript/basico.js
@@ -1,10 +1,30 @@
 "use strict";
 
-$("#donGallevante").click(function()
+function cargarFirebase()
 {
+   // Inicializo mi base de datos Realtime Database
    var firebaseDB = firebase.database();
+   
    // Para cargar el item raíz de mi DB, es con firebaseDB.ref() con parámetro en blanco
-   var datos = firebaseDB.ref().child("kk").get();
+   // WARNING: Esto baja toda la base de datos con todo y todo, y Google cobra eso carizzimo
+   // de París
+   var datos_promesa = firebaseDB.ref().child("kk").get();
+
+   // Pero todavía no puedo usar esos datos, porque vienen en forma de promesa. Es necesario
+   // esperar a que la promesa se cumpla (que es cuando ya se cargaron) y extraer su
+   // valor.
+   // then() es una función que recibe como parámetro una función que se corre cuando
+   // la promesa se cumple
+   var datos = null;
+   datos_promesa.then(function(snapshot)
+   {
+      if (snapshot.exists())
+      {
+         datos = snapshot.val();
+      }
+   });
+
+   // Ahora sí, presento mis datos
    $(this).text(`firebaseDB[kk] = ${datos}`);
 });
 
@@ -24,4 +44,14 @@ function webworker()
          console.log(textito);
       }
    }
+}
+
+function guardarFirebaseNuevo()
+{
+
+}
+
+function guardarFirebaseSobreescribir()
+{
+
 }
