ac23741a3470a2507bac9c6823e8a72d2494445c
Zacatenango <zacatenango@lalala>
2021-04-07 23:39:27 -0500

    Avanzando ejemplo de notificaciones

--- docs/javascript/notificaciones.js
+++ docs/javascript/notificaciones.js
@@ -1,3 +1,16 @@
 "use strict";
 
+$("#btnNotificacion").click(iNotify);
 
+function iNotify()
+{
+   if (!("Notification" in window))
+   {
+      alert("Su Internet Explorer no soporta notificaciones");
+      return;
+   }
+   else if (Notification.permission === "granted")
+   {
+      var notificacion = new Notification("Don Guilluvina", { body: "Don Disersa" });
+   }
+}
