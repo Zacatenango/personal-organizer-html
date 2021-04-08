fc879aca9467722590a599b56f7185e21332a5c2
Zacatenango <zacatenango@lalala>
2021-04-08 01:30:38 -0500

    Comentarios de explicación de notificaciones

--- docs/javascript/notificaciones.js
+++ docs/javascript/notificaciones.js
@@ -2,6 +2,7 @@
 
 function iNotify()
 {
+   // Paso 1: confirmar que nuesto navegador sí maneje notificaciones
    if (!("Notification" in window))
    {
       alert("Su Internet Explorer no soporta notificaciones");
@@ -9,15 +10,20 @@ function iNotify()
    }
    else 
    {
+      // Paso 2: si sí las maneja, confirmar que haya permiso.
       if (Notification.permission === "granted")
       {
+         // Paso 3: si hay permiso, mostrar la notificación
+         // Sintaxis: var <objeto de notificacion> = new Notification("<titulo del mensaje>", { body: "<contenido del mensaje>" });
          var notificacion = new Notification("Don Guilluvina", { body: "Don Disersa" });
       }
       else
       {
+         // Paso 4: si no hay permiso, lo pido
          Notification.requestPermission();
       }
    }
 }
 
+// Por seguridad, las notificaciones se tienen que pedir obligatoriamente en un event handler
 $("#btnNotificacion").click(iNotify);
